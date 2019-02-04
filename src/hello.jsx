ddae3f162d846da0c70f82cec7df5e6dd40955c2
nevroje <joakim.berglind@gmail.com>
2019-02-04 14:24:00 +0100

    Removed spelling error and fixed footer-wrapper in css

--- src/hello.jsx
+++ src/hello.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Hello = () => {
         return (
-           <p className="content-text">Hello World</p> 
+           <p className="content-text">Hello World</p>
         )
 }
 
