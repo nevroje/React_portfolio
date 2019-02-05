c6ca8519a93af666ec0fcd1b1a8710c58ab33cee
nevroje <joakim.berglind@gmail.com>
2019-02-05 15:32:02 +0100

    merge fix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Hello from "./hello";
+import CitiesSlider from "./hello";
 import Header from "./header";
 import Footer from "./footer";
 import "./css/tailwind.css";
+import "./css/sliders.css";
 import {BrowserRouter} from "react-router-dom";
 import Projects from "./projects";
 import About from "./about";
 import {Switch, Route} from "react-router-dom";
 
 const App = () => {
+    const slides = [
+        {
+          city: 'Lorem',
+          country: 'Lorem ipsum dolor sit amet.',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/paris.jpg',
+        },
+        {
+          city: 'Lorem',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/singapore.jpg',
+        },
+        {
+          city: 'Lorem',
+          country: 'Lorem ipsum dolor sit amet.',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg',
+        },
+        {
+          city: 'Lorem',
+          country: 'Lorem ipsum dolor sit amet.',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/amsterdam.jpg',
+        },
+        {
+          city: 'Lorem',
+          country: 'Lorem ipsum dolor sit amet.',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/moscow.jpg',
+        },
+      ];
+
     return (
         <div className="page-wrapper">
             <div className="page-content">
                 <Header/>
                     <div className="content-wrapper">
                         <Switch>
-                            <Route exact path='/' component={Hello}></Route>
+                            <Route exact path='/' component={() => <CitiesSlider slides={slides}/>}></Route>
                                 <Route exact path='/about' component={About}></Route>
                             <Route exact path='/projects' component={Projects}></Route>
                         </Switch>
