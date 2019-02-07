a6be5706b10994df502e9d72d7f5b5e6140b0254
nevroje <joakim.berglind@gmail.com>
2019-02-07 11:08:04 +0100

    changed uppercase letters in footer.jsx to match

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "React"
+import React, {Component} from "react"
 
 const Footer = () => {
     return (
--- src/header.jsx
+++ src/header.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "React";
+import React, {Component} from "react";
 import {NavLink, Link} from "react-router-dom";
 
 const Header = () => {
