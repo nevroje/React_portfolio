00cbb8ceb1c6085671dc7ffe04e4199b93099f0a
nevroje <joakim.berglind@gmail.com>
2019-02-08 12:01:52 +0100

    added more text to about.me

--- src/about.jsx
+++ src/about.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <h1 className="content-text">About Me</h1>
             <p className="content-text"></p>
             <br></br>
-            <p className="content-text">My name is Joakim and i am a up and coming Full Stack Web Dev with a burning passion for code.</p>
+            <p className="content-text">My name is Joakim and i am a up and coming Full Stack Web Dev studen at Craft Academy with a burning passion for code.</p>
         </div>
     )
 }
